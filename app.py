0f5d36e78d54c418a4ea7aa9be222fcd7afda49f
Yuvraj Singh Mor <140009504+yuvimor@users.noreply.github.com>
2023-10-02 04:07:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
+from transformers import MarianTokenizer, MarianMTModel
 import streamlit as st
-from transformers import AutoModelForSeq2SeqLM
 
-# Load the pre-trained Transformer model
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+st.title('French <--> English Translator')
+text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
 
-# Define a function to translate a French sentence into English
-def translate(sentence):
+if st.button('Translate to English'):
+    if text == '':
+        st.write('Please enter French text for translation') 
+    else: 
+        fr_en_translation_model_name = 'Helsinki-NLP/opus-mt-fr-en'
+        fr_en_model = MarianMTModel.from_pretrained(fr_en_translation_model_name)
+        fr_en_tokenizer = MarianTokenizer.from_pretrained(fr_en_translation_model_name)
+        fr_en_batch = fr_en_tokenizer.prepare_seq2seq_batch(src_texts=[text])
+        fr_en_gen = fr_en_model.generate(**fr_en_batch)
+        fr_en_translation = fr_en_tokenizer.batch_decode(fr_en_gen, skip_special_tokens=True)
+        st.write('', str(fr_en_translation).strip('][\''))
+else: pass
 
-  # Convert the French sentence to a tensor
-  sentence_tensor = torch.tensor(sentence)
-  
-  translated_sentence = model.generate(input_ids=sentence, max_length=128)
-  return translated_sentence[0]
-
-# Create a Streamlit app
-st.title("French to English Translator")
-
-# Get the French sentence from the user
-french_sentence = st.text_input("Enter a French sentence:")
-
-# Translate the French sentence into English
-english_sentence = translate(french_sentence)
-
-# Display the translated sentence to the user
-st.write("English translation:", english_sentence)
+if st.button('Translate to French'):
+    if text == '':
+        st.write('Please enter English text for translation') 
+    else: 
+        en_fr_translation_model_name = 'Helsinki-NLP/opus-mt-en-fr'
+        en_fr_model = MarianMTModel.from_pretrained(en_fr_translation_model_name)
+        en_fr_tokenizer = MarianTokenizer.from_pretrained(en_fr_translation_model_name)
+        en_fr_batch = en_fr_tokenizer.prepare_seq2seq_batch(src_texts=[text])
+        en_fr_gen = en_fr_model.generate(**en_fr_batch)
+        en_fr_translation = en_fr_tokenizer.batch_decode(en_fr_gen, skip_special_tokens=True)
+        st.write('', str(en_fr_translation).strip('][\''))
+else: pass
